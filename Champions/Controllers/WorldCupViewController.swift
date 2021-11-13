662eaa20891c0512af7c0d1feecfe87927c9f9ce
Italo <italord.melo@gmail.com>
2021-11-13 19:01:19 -0300

    passing data to the next screen, adding final game details

--- Champions/Controllers/WorldCupViewController.swift
+++ Champions/Controllers/WorldCupViewController.swift
@@ -8,11 +8,31 @@
 import UIKit
 
 class WorldCupViewController: UIViewController {
+    
+    var worldCup : WorldCupStruct!
 
+    @IBOutlet weak var ivWinner: UIImageView!
+    @IBOutlet weak var ivVice: UIImageView!
+    @IBOutlet weak var lbScore: UILabel!
+    @IBOutlet weak var lbWinner: UILabel!
+    @IBOutlet weak var lbVice: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupTitle()
+        setupData()
+    }
+    
+    private func setupTitle() {
+        navigationItem.title = worldCup.year.stringValue
+    }
+    
+    private func setupData(){
+        ivWinner.image = UIImage(named: worldCup.winner)
+        ivVice.image = UIImage(named: worldCup.vice)
+        lbScore.text = "\(worldCup.winnerScore) x \(worldCup.viceScore)"
+        lbWinner.text = worldCup.winner
+        lbVice.text = worldCup.vice
+        
     }
 
 }
