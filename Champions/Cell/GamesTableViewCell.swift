221594820e0ebdff4f3211f55336fb4e26833487
Italo <italord.melo@gmail.com>
2021-11-13 21:01:38 -0300

    added tableview with champions games on sections

---
+++ Champions/Cell/GamesTableViewCell.swift
@@ -0,0 +1,35 @@
+//
+//  GamesTableViewCell.swift
+//  Champions
+//
+//  Created by Aluno on 13/11/21.
+//
+
+import UIKit
+
+class GamesTableViewCell: UITableViewCell {
+    @IBOutlet weak var ivHome: UIImageView!
+    @IBOutlet weak var ivAway: UIImageView!
+    @IBOutlet weak var lbHome: UILabel!
+    @IBOutlet weak var lbAway: UILabel!
+    @IBOutlet weak var lbScore: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+    }
+    
+    func prepare(with game : GameStruct) {
+        ivHome.image = UIImage(named: game.home)
+        ivAway.image = UIImage(named: game.away)
+        lbHome.text = game.home
+        lbAway.text = game.away
+        lbScore.text = game.score
+    }
+
+}
--- Champions/Controllers/WorldCupViewController.swift
+++ Champions/Controllers/WorldCupViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class WorldCupViewController: UIViewController {
+class WorldCupViewController: UIViewController , UITableViewDataSource, UITableViewDelegate {
     
     var worldCup : WorldCupStruct!
 
@@ -16,10 +16,35 @@ class WorldCupViewController: UIViewController {
     @IBOutlet weak var lbScore: UILabel!
     @IBOutlet weak var lbWinner: UILabel!
     @IBOutlet weak var lbVice: UILabel!
+    @IBOutlet weak var tvMatches: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         setupTitle()
         setupData()
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return worldCup.matches.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let games = worldCup.matches[section].games
+        return games.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let match = worldCup.matches[indexPath.section]
+        let game = match.games[indexPath.row]
+             
+        let cell = tableView.dequeueReusableCell(withIdentifier: "GamesCell", for: indexPath) as! GamesTableViewCell
+        cell.prepare(with: game)
+             
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        let match = worldCup.matches[section]
+        return match.stage
     }
     
     private func setupTitle() {
